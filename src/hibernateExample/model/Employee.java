dbfbf9dab988e3da116cfb2f22443a4a18791293
rabdul <rabdul@xpressdocs.com>
2017-05-17 15:11:29 -0500

    added spring mvc resp api code

--- src/hibernateExample/model/Employee.java
+++ src/hibernateExample/model/Employee.java
@@ -7,13 +7,22 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 @Entity
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties({"firstName"})
+@JsonPropertyOrder({"lastName","firstName","empId"})
 @Table(name="Employee_details")
 public class Employee {
 
 	@Id
 	@Column
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@JsonProperty(value="emp_id")
 	private int empId;
 	
 	
@@ -24,9 +33,11 @@ public class Employee {
 		this.empId = empId;
 	}
 	@Column
+	@JsonProperty(value="first_name")
 	private String firstName;
 	
 	@Column
+	@JsonProperty(value="last_name")
 	private String lastName;
 	
 	public String getFirstName() {
